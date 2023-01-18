1faee2d71dcaf68a9530e5a83c832e1b04c9c380
jquery404 <jquery404@gmail.com>
2023-01-18 19:10:46 +1300

    new project details

---
+++ apps/foodie/food.php
@@ -0,0 +1,17 @@
+<?php
+
+echo `{
+    "record": [
+      {
+        "id": 1,
+        "title": "Kilsds",
+        "thumb": "https://via.placeholder.com/150/0000FF/808080"
+      },
+      {
+        "id": 2,
+        "title": "Sad",
+        "thumb": "https://via.placeholder.com/150/FFFF00/000000"
+      }
+    ]
+  }`;
+?>
