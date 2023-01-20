3b637f1b7336651d77762b359ec1d20c03dc1fac
jquery404 <jquery404@gmail.com>
2023-01-20 16:25:04 +1300

    something to start

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Backend from './components/Backend';
+import GrabGetGo from './components/GrabGetGo';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
+          <Route exact path="/grabgetgo" element={<GrabGetGo />} />
           <Route exact path="/api" element={<Backend />} />
       </Routes>
     </BrowserRouter>
--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,13 @@ function About() {
     return (
         <>
             <Layout>
-                <h1>About Page</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <h1>About Page</h1>
+                        </div>
+                    </div>
+                </div>
             </Layout>
         </>
     )
